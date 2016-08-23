b3e99e22a3bc354ea0d897456bf09d9708515dfa
Maurizio Napolitano <napo@fbk.eu>
2016-08-23 12:27:58 +0200

    update students

--- students/_posts/create_students.py
+++ students/_posts/create_students.py
@@ -14,7 +14,11 @@ reader = unicodecsv.reader(f, encoding='iso-8859-1')
 k = 0
 for row in reader:
     if k > 0:
-        filename = datetime.today().strftime('%Y-%m-%d')
+        yearmonthday = datetime.today().strftime('%Y-%m-%d')
+        yearmonthday = "2016-08-23"
+        daymonthyear = datetime.today().strftime('%d/%m/%Y')
+        daymonthyear = "23/08/2016"
+        filename = yearmonthday
         filename += "-" + str(row[0].encode('iso-8859-1')).lower().replace(" ","_") + ".md"
         new_yaml = open(filename, 'w')
         yaml_text = ""
@@ -22,15 +26,15 @@ for row in reader:
         yaml_text += "layout: default \n"
         surname =  str(row[0].encode('iso-8859-1'))
         name = str(row[1].encode('iso-8859-1'))
-        yaml_text += "id: " + datetime.today().strftime('%Y-%m-%d') + "-" + surname.replace(" ","_") + "-" + name.replace(" ","_") + "\n"
-        yaml_text += "surname: " +  surname + "\n"
+        yaml_text += "id: " + yearmonthday + "-" + surname.replace(" ","_") + "-" + name.replace(" ","_") + "\n"
+        yaml_text += "surname: " +  surname.replace("\n",""), + "\n"
         yaml_text += "name: " + name  + "\n"
         yaml_text += "university: " + str(row[2].encode('iso-8859-1'))  + "\n"
-        yaml_text += "date: " + datetime.today().strftime('%d/%m/%Y')  + "\n"
-        yaml_text += "aboutme: " +  str(row[3].encode('iso-8859-1'))  + "\n"
+        yaml_text += "date: " + daymonthyear + "\n"
+        yaml_text += "aboutme: " +  str(row[3].encode('iso-8859-1')).replace("\n","<br/>")  + "\n"
         yaml_text += "from: " +  str(row[4].encode('iso-8859-1'))  + "\n"
         yaml_text += "research_topic: " +  str(row[5].encode('iso-8859-1'))  + "\n"
-        yaml_text += "abstract: " + str(row[6].encode('iso-8859-1'))  + "\n"
+        yaml_text += "abstract: " + str(row[6].encode('iso-8859-1')).replace("\n","<br/>")  + "\n"
         yaml_text += "advisor: "  + str(row[7].encode('iso-8859-1'))  + "\n"
         yaml_text += "keywords: " + str(row[8].encode('iso-8859-1'))  + "\n"
         website = str(row[9].encode('iso-8859-1'))
--- students/_posts/deploy_students.sh
+++ students/_posts/deploy_students.sh
@@ -1,4 +1,6 @@
-rm *.md
+wget "https://docs.google.com/spreadsheets/d/1UJ8eHwcBsdKRjwc6uu2KVhNIn_FSEpLMh6xqMNQ5sUY/pub?gid=191666891&single=true&output=csv" -O students.csv 
+mkdir -p old
+mv *.md old
 git pull
 python create_students.py
 git commit -a -m "update students"
