cd799fcc67381d5f5d9962479cb4da195142d245
Maurizio Napolitano <napo@fbk.eu>
2017-03-15 11:55:57 +0100

    First pages commit

--- affiliations/_posts/2014-07-17-universitatrento-physics.markdown
+++
@@ -1,9 +0,0 @@
----
-title: Department of physics
-website: http://www.physics.unitn.it/
-layout: default
-tag: unitn_department
-phdschool: Physics
-phdschool_link: http://web.unitn.it/en/drphys
-reference_person: 
----
--- bin/deploy_alumni.sh
+++
@@ -1,9 +0,0 @@
-cd ../alumni/_posts
-wget "https://docs.google.com/spreadsheets/d/1UJ8eHwcBsdKRjwc6uu2KVhNIn_FSEpLMh6xqMNQ5sUY/pub?gid=2103651667&single=true&output=csv" -O alumni.csv 
-mkdir -p old
-mv *.md old
-git pull
-python ../../bin/create_alumni.py
-git add *.md
-git commit -a -m "update alumni"
-git push 
